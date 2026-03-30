ba7187abd19f50d167f1673afba3c2722d3814ec
kashef7 <11410120230477@stud.cu.edu.eg>
2026-03-30 03:13:42 +0200

    refactor: remove unused and deprecated utility modules

--- testFrontEndForSocket/config.js
+++
@@ -1,5 +0,0 @@
-export const CONFIG = {
-    API_BASE_URL: "http://127.0.0.1:3000/api/v1",
-    SOCKET_URL: "http://127.0.0.1:3000",
-    GOOGLE_AUTH_URL: "http://localhost:3000/api/v1/auth/google"
-};
