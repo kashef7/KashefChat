a13c256bbc2b2d4b2a7b9361f1756b501b4854c5
kashef7 <11410120230477@stud.cu.edu.eg>
2026-04-13 10:21:58 +0200

    feat: add configuration constants for API, socket, and authentication endpoints

--- frontEnd/snuggle-chat-30-main/src/config/index.ts
+++ frontEnd/snuggle-chat-30-main/src/config/index.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  API_BASE_URL: "https://chatapi.kashefchat.cc/api/v1",
-  SOCKET_URL: "https://chatapi.kashefchat.cc",
-  GOOGLE_AUTH_URL: "https://chatapi.kashefchat.cc/api/v1/auth/google",
+  API_BASE_URL: "https://chatapi.kashefapps.cc/api/v1",
+  SOCKET_URL: "https://chatapi.kashefapps.cc",
+  GOOGLE_AUTH_URL: "https://chatapi.kashefapps.cc/api/v1/auth/google",
 };
